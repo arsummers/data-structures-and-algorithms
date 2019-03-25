59b068ae969563087fd0cad490993c52480c0035
Aliya Summers <aliyasummers1@gmail.com>
2019-03-24 23:51:34 -0700

    only 1 working

--- code-challenges/301/challenge-10-chain/challenges-10.test.js
+++ code-challenges/301/challenge-10-chain/challenges-10.test.js
@@ -36,6 +36,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+
+  let arrSum = function(input){
+    return input.reduce(function(a,b){
+      return a + b
+    }, 0);
+  }
+  return input;
 };
 
 /* ------------------------------------------------------------------------------------------------
